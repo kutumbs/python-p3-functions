86e7fb0a4c9b299b2422a4d8b694b054c164a8bb
jonathan <jonathan.123kutumbakana@gmail.com>
2024-03-18 11:13:05 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    pass    
+    return print("Hello, programmer!")
 
+
+def greet(name="jonathan"):
+    return print(f"hello,{name}!")
 def greet(name):
     pass
 
 def greet_with_default(name="programmer"):
     pass
+    return print(f"hello,{name}!")
 
 def add(num1, num2):
     pass
-
+    num1 = 45
+    num2 = 66
+    sum = num1+num2
+    return sum
 def halve(number):
     pass
+    return number/2
